e5aeef3989027328364c97aca6dece5614844cc8
nizihad824 <mdnurulislam97@gmail.com>
2022-09-28 09:04:56 +0200

    CRUD operation mongodb

---
+++ src/components/Home/Home.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+
+const Home = () => {
+    const [users,setUsers]=useState([]);
+    useEffect(()=>{
+        fetch('http://localhost:5000/user')
+        .then(res => res.json())
+        .then(data =>setUsers(data))
+    },[])
+
+    const handleRemoveUser = id =>{
+
+        const proceed =window.confirm('Are you sure  you want to delete this user??')
+        if(proceed){
+            console.log('deleting user with id')
+            const url =`http://localhost:5000/user/${id}`
+            fetch(url,{
+                method : 'DELETE'
+
+            })
+            .then(res =>res.json())
+            .then(data =>{
+                if(data.deletedCount >0){
+                    console.log('Deleted')
+                    const remaining = users.filter(user => user._id !== id );
+                    setUsers(remaining)
+                }
+            })
+        }
+        
+    }
+
+    return (
+        <div>
+            <h1>Available Users :{users.length}</h1>
+            <ul>
+                {
+                    users.map(user => <li
+                    key={user._id}
+                    >{user.name}::{user.email}
+                    <button onClick={()=>handleRemoveUser(user._id)}>X</button>
+                    </li>
+
+                    
+                    )
+                }
+            </ul>
+            
+        </div>
+    );
+};
+
+export default Home;
